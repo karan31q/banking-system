d1f918d653d5dd335c3178e08578754e7c74ff59
Karandeep Singh <karan31q@gmail.com>
2024-10-17 09:40:22 +0530

    add a helper class to get user details

---
+++ src/main/java/dev/imarti/bank/util/FetchUserDetails.java
@@ -0,0 +1,60 @@
+package dev.imarti.bank.util;
+
+import dev.imarti.bank.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FetchUserDetails {
+    public String userID;
+    public String name;
+    public String email;
+    public int balance;
+
+    public FetchUserDetails(String userID) {
+        this.userID = userID;
+        try (Connection connection = DBConnection.connectDB()) {
+            String query = "SELECT * FROM users WHERE user_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, userID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                this.name = resultSet.getString("name");
+                this.email = resultSet.getString("email");
+                try {
+                    String accountsQuery = "SELECT * from accounts where user_id = ?";
+                    PreparedStatement accountsPreparedStatement = connection.prepareStatement(accountsQuery);
+                    accountsPreparedStatement.setString(1, userID);
+                    ResultSet accountsResultSet = accountsPreparedStatement.executeQuery();
+                    if (accountsResultSet.next()) {
+                        this.balance = accountsResultSet.getInt("balance");
+                    }
+                } catch (SQLException e) {
+                    System.out.println(e.getMessage());
+                }
+            } else {
+                System.out.println(userID + " not found");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}
