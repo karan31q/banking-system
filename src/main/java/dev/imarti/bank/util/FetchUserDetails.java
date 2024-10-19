6983bb2c90b52b4e2cd6270a3c33c8a3f2520e80
Karandeep Singh <karan31q@gmail.com>
2024-10-19 20:40:08 +0530

    update FetchUserDetails
    * we can get latest value whenever needed
    * i guess this could be done differently but idfc.

--- src/main/java/dev/imarti/bank/util/FetchUserDetails.java
+++ src/main/java/dev/imarti/bank/util/FetchUserDetails.java
@@ -14,36 +14,14 @@ public class FetchUserDetails {
     public String name;
     public String email;
     public int balance;
-    public int accountID;
+    public String accountID;
 
     public FetchUserDetails(String userID) {
         this.userID = userID;
-        try (Connection connection = DBConnection.connectDB()) {
-            String query = "SELECT * FROM users WHERE user_id = ?";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, userID);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                this.name = resultSet.getString("full_name");
-                this.email = resultSet.getString("email");
-                try {
-                    String accountsQuery = "SELECT * from accounts where user_id = ?";
-                    PreparedStatement accountsPreparedStatement = connection.prepareStatement(accountsQuery);
-                    accountsPreparedStatement.setString(1, userID);
-                    ResultSet accountsResultSet = accountsPreparedStatement.executeQuery();
-                    if (accountsResultSet.next()) {
-                        this.balance = accountsResultSet.getInt("balance");
-                        this.accountID = accountsResultSet.getInt("account_id");
-                    }
-                } catch (SQLException e) {
-                    System.out.println(e.getMessage());
-                }
-            } else {
-                System.out.println(userID + " not found");
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        getName();
+        getEmail();
+        getBalance();
+        getAccountID();
     }
 
     public String getUserID() {
@@ -51,18 +29,75 @@ public class FetchUserDetails {
     }
 
     public String getName() {
+        try (Connection connection = DBConnection.connectDB()) {
+            String query = "SELECT full_name FROM users WHERE user_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, userID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                this.name = resultSet.getString("full_name");
+                return this.name;
+            } else {
+                System.out.println(userID + " not found");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return name;
     }
 
     public String getEmail() {
+        try (Connection connection = DBConnection.connectDB()) {
+            String query = "SELECT email FROM users WHERE user_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, userID);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                this.email = resultSet.getString("email");
+                return this.email;
+            } else {
+                System.out.println(userID + " not found");
+                return null;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return email;
     }
 
     public int getBalance() {
+        try (Connection connection = DBConnection.connectDB()) {
+            String accountsQuery = "SELECT balance from accounts where user_id = ?";
+            PreparedStatement accountsPreparedStatement = connection.prepareStatement(accountsQuery);
+            accountsPreparedStatement.setString(1, userID);
+            ResultSet accountsResultSet = accountsPreparedStatement.executeQuery();
+            if (accountsResultSet.next()) {
+                this.balance = accountsResultSet.getInt("balance");
+                return this.balance;
+            } else {
+                System.out.println(userID + " not found");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return balance;
     }
 
-    public int getAccountID() {
+    public String getAccountID() {
+        try (Connection connection = DBConnection.connectDB()) {
+            String accountsQuery = "SELECT account_id from accounts where user_id = ?";
+            PreparedStatement accountsPreparedStatement = connection.prepareStatement(accountsQuery);
+            accountsPreparedStatement.setString(1, userID);
+            ResultSet accountsResultSet = accountsPreparedStatement.executeQuery();
+            if (accountsResultSet.next()) {
+                this.accountID = accountsResultSet.getString("account_id");
+                return this.accountID;
+            } else {
+                System.out.println(userID + " not found");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return accountID;
     }
 
